762d0f603b7d50ef042aee73f142dc83915ff4f8
Roberto R <robertor12@outlook.cl>
2025-11-30 05:37:50 -0300

    registro de pacientes ;)

---
+++ FormRegistrosPacientes.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Clinica_de_salud.Vistas
+{
+    partial class FormRegistrosPacientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            txtpre = new TextBox();
+            label10 = new Label();
+            dtpfe = new DateTimePicker();
+            btnRegistro = new Button();
+            btnregistrar = new Button();
+            cmbseso = new ComboBox();
+            txttipoSangre = new TextBox();
+            txtDireccion = new TextBox();
+            txtCorreo = new TextBox();
+            txtTelefono = new TextBox();
+            txtcontraseña = new TextBox();
+            txtrut = new TextBox();
+            txtNombre = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label11 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtpre);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(dtpfe);
+            groupBox1.Controls.Add(btnRegistro);
+            groupBox1.Controls.Add(btnregistrar);
+            groupBox1.Controls.Add(cmbseso);
+            groupBox1.Controls.Add(txttipoSangre);
+            groupBox1.Controls.Add(txtDireccion);
+            groupBox1.Controls.Add(txtCorreo);
+            groupBox1.Controls.Add(txtTelefono);
+            groupBox1.Controls.Add(txtcontraseña);
+            groupBox1.Controls.Add(txtrut);
+            groupBox1.Controls.Add(txtNombre);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(581, 396);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // txtpre
+            // 
+            txtpre.Location = new Point(143, 229);
+            txtpre.Name = "txtpre";
+            txtpre.Size = new Size(202, 23);
+            txtpre.TabIndex = 44;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(22, 293);
+            label10.Name = "label10";
+            label10.Size = new Size(120, 15);
+            label10.TabIndex = 43;
+            label10.Text = "Fecha de nacimiento:";
+            // 
+            // dtpfe
+            // 
+            dtpfe.Location = new Point(143, 287);
+            dtpfe.Name = "dtpfe";
+            dtpfe.Size = new Size(211, 23);
+            dtpfe.TabIndex = 42;
+            // 
+            // btnRegistro
+            // 
+            btnRegistro.BackColor = Color.IndianRed;
+            btnRegistro.Location = new Point(280, 316);
+            btnRegistro.Name = "btnRegistro";
+            btnRegistro.Size = new Size(295, 70);
+            btnRegistro.TabIndex = 41;
+            btnRegistro.Text = "Cancelar registro";
+            btnRegistro.UseVisualStyleBackColor = false;
+            btnRegistro.Click += btnRegistro_Click;
+            // 
+            // btnregistrar
+            // 
+            btnregistrar.BackColor = SystemColors.ActiveCaption;
+            btnregistrar.Location = new Point(-5, 317);
+            btnregistrar.Name = "btnregistrar";
+            btnregistrar.Size = new Size(295, 70);
+            btnregistrar.TabIndex = 40;
+            btnregistrar.Text = "Completar registro";
+            btnregistrar.UseVisualStyleBackColor = false;
+            btnregistrar.Click += btnregistrar_Click;
+            // 
+            // cmbseso
+            // 
+            cmbseso.FormattingEnabled = true;
+            cmbseso.Location = new Point(143, 258);
+            cmbseso.Name = "cmbseso";
+            cmbseso.Size = new Size(200, 23);
+            cmbseso.TabIndex = 38;
+            // 
+            // txttipoSangre
+            // 
+            txttipoSangre.Location = new Point(143, 200);
+            txttipoSangre.Name = "txttipoSangre";
+            txttipoSangre.Size = new Size(202, 23);
+            txttipoSangre.TabIndex = 35;
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Location = new Point(143, 171);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(202, 23);
+            txtDireccion.TabIndex = 34;
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Location = new Point(143, 142);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(202, 23);
+            txtCorreo.TabIndex = 33;
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(143, 113);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(202, 23);
+            txtTelefono.TabIndex = 32;
+            // 
+            // txtcontraseña
+            // 
+            txtcontraseña.Location = new Point(143, 84);
+            txtcontraseña.Name = "txtcontraseña";
+            txtcontraseña.Size = new Size(202, 23);
+            txtcontraseña.TabIndex = 31;
+            // 
+            // txtrut
+            // 
+            txtrut.Location = new Point(143, 55);
+            txtrut.Name = "txtrut";
+            txtrut.Size = new Size(202, 23);
+            txtrut.TabIndex = 30;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(143, 26);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(202, 23);
+            txtNombre.TabIndex = 29;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(22, 266);
+            label9.Name = "label9";
+            label9.Size = new Size(34, 15);
+            label9.TabIndex = 28;
+            label9.Text = "Sexo:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(22, 237);
+            label8.Name = "label8";
+            label8.Size = new Size(58, 15);
+            label8.TabIndex = 27;
+            label8.Text = "Prevision:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(19, 208);
+            label7.Name = "label7";
+            label7.Size = new Size(89, 15);
+            label7.TabIndex = 26;
+            label7.Text = "Tipo de Sangre:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(19, 179);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 15);
+            label6.TabIndex = 25;
+            label6.Text = "Direccion:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(22, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 15);
+            label5.TabIndex = 24;
+            label5.Text = "Correo:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(22, 121);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 15);
+            label4.TabIndex = 23;
+            label4.Text = "Telefono:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 15);
+            label3.TabIndex = 22;
+            label3.Text = "Contraseña:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 15);
+            label2.TabIndex = 21;
+            label2.Text = "Rut:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(19, 34);
+            label11.Name = "label11";
+            label11.Size = new Size(54, 15);
+            label11.TabIndex = 20;
+            label11.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 0;
+            // 
+            // FormRegistrosPacientes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(605, 416);
+            Controls.Add(groupBox1);
+            Name = "FormRegistrosPacientes";
+            Text = "Registro de paciente";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private ComboBox cmbseso;
+        private TextBox txttipoSangre;
+        private TextBox txtDireccion;
+        private TextBox txtCorreo;
+        private TextBox txtTelefono;
+        private TextBox txtcontraseña;
+        private TextBox txtrut;
+        private TextBox txtNombre;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label11;
+        private Button btnRegistro;
+        private Button btnregistrar;
+        private Label label10;
+        private DateTimePicker dtpfe;
+        private TextBox txtpre;
+    }
+}
---
+++ FormRegistrosPacientes.cs
@@ -0,0 +1,110 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clinica_de_salud;
+
+namespace Clinica_de_salud.Vistas
+{
+    public partial class FormRegistrosPacientes : Form
+    {
+        public FormRegistrosPacientes()
+        {
+            InitializeComponent();
+            LimpiarCampos();
+            cmbseso.Items.Add("masculino");
+            cmbseso.Items.Add("femenino");
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = string.Empty;
+            txtrut.Text = string.Empty;
+            txtcontraseña.Text = string.Empty;
+            txtTelefono.Text = string.Empty;
+            txtCorreo.Text = string.Empty;
+            txtDireccion.Text = string.Empty;
+            dtpfe.Value = DateTime.Today;
+            txttipoSangre.Text = string.Empty;
+            cmbseso.Text = string.Empty;
+            txtpre.Text = string.Empty;
+        }
+
+        private void btnregistrar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string rut = txtrut.Text.Trim();
+            string contraseña = txtcontraseña.Text.Trim();
+            DateTime fecha = dtpfe.Value;
+            string sexo = cmbseso.Text.Trim();
+            string direccion = txtDireccion.Text.Trim();
+            string telefono = txtTelefono.Text.Trim();
+            string email = txtCorreo.Text.Trim();
+            string prevision = txtpre.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(contraseña) || string.IsNullOrEmpty(sexo) || string.IsNullOrEmpty(direccion))
+            {
+                MessageBox.Show("Todos los campos son obligatorios", "Validación", MessageBoxButtons.OK);
+                return;
+            }
+
+            string rol_usuario = "paciente";
+            string contraseñaAdo = BCrypt.Net.BCrypt.HashPassword(contraseña);
+
+            try
+            {
+                using (var conn = Conexion.ObtenerConexion())
+                {
+                    conn.Open();
+                    string queryPacientes = "INSERT INTO pacientes (nombre_completo, rut, fecha_nacimiento, sexo_genero, direccion, telefono, email, sistema_salud, fecha_registro) VALUES (@nombre_completo, @rut, @fecha_nacimiento, @sexo_genero, @direccion, @telefono, @email, @sistema_salud, @fecha_registro)";
+
+                    MySqlCommand cmdPacientes = new MySqlCommand(queryPacientes, conn);
+                    cmdPacientes.Parameters.AddWithValue("@nombre_completo", nombre);
+                    cmdPacientes.Parameters.AddWithValue("@rut", rut);
+                    cmdPacientes.Parameters.AddWithValue("@fecha_nacimiento", fecha);
+                    cmdPacientes.Parameters.AddWithValue("@sexo_genero", sexo);
+                    cmdPacientes.Parameters.AddWithValue("@direccion", direccion);
+                    cmdPacientes.Parameters.AddWithValue("@telefono", telefono);
+                    cmdPacientes.Parameters.AddWithValue("@email", email);
+                    cmdPacientes.Parameters.AddWithValue("@sistema_salud", prevision);
+                    cmdPacientes.Parameters.AddWithValue("@fecha_registro", DateTime.Now);
+
+                    cmdPacientes.ExecuteNonQuery();
+
+                    string queryUsuarios = "INSERT INTO usuarios (rut, nombre, email, password, rol) VALUES (@rutUsuario, @nombreUsuario, @emailUsuario, @password, @rol)";
+
+                    MySqlCommand cmdUsuarios = new MySqlCommand(queryUsuarios, conn);
+                    cmdUsuarios.Parameters.AddWithValue("@rutUsuario", rut);
+                    cmdUsuarios.Parameters.AddWithValue("@nombreUsuario", nombre);
+                    cmdUsuarios.Parameters.AddWithValue("@emailUsuario", email);
+                    cmdUsuarios.Parameters.AddWithValue("@password", contraseñaAdo);
+                    cmdUsuarios.Parameters.AddWithValue("@rol", rol_usuario);
+
+                    cmdUsuarios.ExecuteNonQuery();
+                    MessageBox.Show("Registro completo", "Éxito", MessageBoxButtons.OK);
+                    LimpiarCampos();
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrio un error ado: {ex.Message}", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnRegistro_Click(object sender, EventArgs e)
+        {
+            FormLogin login = new FormLogin();
+            MessageBox.Show("Da solicitud de degisto se cancedo", "valicacion", MessageBoxButtons.OK);
+            this.Close();
+        }
+    }
+}
+
